1845bef23539ef45eb7c640f85697b75f20127e2
mcdonnez <mailman966@gmail.com>
2015-06-15 04:58:14 +0000

    update helper

--- helpers/render.php
+++ helpers/render.php
@@ -2,7 +2,7 @@
 	/*function to render the templates*/
 function render($template, $data = array())
 {
-	$path = '../view/' . $template . '.php';
+	$path = 'view/' . $template . '.php';
 	if (file_exists($path))
 	{
 		extract($data);
